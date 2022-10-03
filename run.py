85b26a18e2aa457b0232910f2782ff39d93f9e60
karol wojcik <karolwojcik29@gmail.com>
2022-10-03 12:17:59 +0100

    Adding Space

---
+++ run.py
@@ -0,0 +1,18 @@
+from money_model import *
+import numpy as np
+from matplotlib import pyplot as plt
+
+model = MoneyModel(50, 10, 10)
+for i in range(20):
+    model.step()
+
+agent_counts = np.zeros((model.grid.width, model.grid.height))
+for cell in model.grid.coord_iter():
+    cell_content, x, y = cell
+    agent_count = len(cell_content)
+    agent_counts[x][y] = agent_count
+plt.imshow(agent_counts, interpolation="nearest")
+plt.colorbar()
+
+# If running from a text editor or IDE, remember you'll need the following:
+plt.show()
